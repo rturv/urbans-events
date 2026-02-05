6d62ceb3eb4df96ef3dd2219e06ead93ddf80ce4
rtur <ramon.tur@soltel.es>
2026-02-05 21:23:08 +0100

    se elimina notificaciones springboot

--- notificaciones/src/main/java/com/urbanevents/notificaciones/domain/NotificacionRepository.java
+++
@@ -1,6 +0,0 @@
-package com.urbanevents.notificaciones.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificacionRepository extends JpaRepository<Notificacion, Long> {
-}
